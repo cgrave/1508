52c06699034213161fc13c6fff01fc41dab8fd5b
Crystal <33204245+cgrave@users.noreply.github.com>
2018-03-07 13:16:44 -0700

    Update stringDateExercise.sql

--- Lesson 23/stringDateExercise.sql
+++ Lesson 23/stringDateExercise.sql
@@ -1,25 +1,27 @@
---String and Date Functions Exercise
 --1. Select the staff names and the name of the month they were hired
-Select firstname + ', ' + LastName'FullNames', datehired, datename(mm,datehired)'month' from staff order by month(datehired)
+Select Firstname + ' '+ lastName 'Staff', datename(mm,Datehired) 'Hire Month' from Staff
 
 --2. How many days did Tess Agonor work for the school?
-Select firstname + ', ' + LastName'FullNames', datediff(dd, datehired, getdate())'hired' from staff where 
+Select DATEDIFF(dd,DateHired, Datereleased) 'Days worked' from Staff
+where FirstName = 'Tess' and lastName ='Agonor'
 
 --3. How Many Students where born in each month? Display the Month Name and the Number of Students.
-
+Select datename(MM,birthdate) 'Month',COUNT (*)'Student Count' from Student 
+group by DATENAME(mm, birthdate)
 
 --4. Select the Names of all the students born in December.
-
+Select FirstName  + ' ' + LastName 'Student Name' from Student
+where datename(mm,Birthdate)='december'
 
 --5. select last three characters of all the courses
+Select RIGHT(courseid,3)'Course Numbers' from Course
 
-
---6. Select the characters in the position description from characters 8 to 13 for PositionID 5
-
+--6. Select the characters from position 8 to 13 for PositionID 5
+Select SUBSTRING(PositionDescription,8,6)'Partial Name' from Position where PositionID = 5
 
 --7. Select all the Student First Names as upper case.
-
+Select upper(FirstName)'Upper Case First Name' from Student
 
 --8. Select the First Names of students whose first names are 3 characters long.
-
+Select FirstName from Student where LEN(firstName) = 3 
 
